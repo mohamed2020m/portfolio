fd17632667e65d7ca9560497ca92dc43ba3c3e42
Leeuw <mohamedessabir20@gmail.com>
2023-07-27 08:39:47 +0100

    fixing some design issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-
+import React, {useEffect, useState} from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import {About, Contact, Experience, Education, Hero, Navbar, Tech, Works, StarsCanvas, EarthCanvas } from './components'
 import ScrollToTop from "react-scroll-to-top";
 
 const App = ()=> {
+  const [isMobile, setIsMobile ] = useState(false)
+
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) =>{
+      setIsMobile(event.matches)
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+
+  },[])
+
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -16,10 +34,10 @@ const App = ()=> {
         <Experience/>
         <Tech/>
         <Works/>
-        <EarthCanvas className='dvGrab' />
+        {!isMobile ? <EarthCanvas className='dvGrab' /> : null}
         <div className='relative z-0'>
           <Contact/>
-          <StarsCanvas/>
+          {!isMobile ? <StarsCanvas/>: null}
         </div>
       </div>
       <ScrollToTop smooth className='flex justify-center items-center hover:bg-gray-300'/>
